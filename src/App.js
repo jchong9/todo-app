28c1e870e37d07c2a020a337477067df63c25746
Jeremy Chong <chong.jeremy@hotmail.com>
2024-06-11 17:07:20 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TaskForm from "./TaskForm";
+
+function App() {
+  return (
+    <div>
+      <TaskForm />
+      <div>
+        <input type='checkbox' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
