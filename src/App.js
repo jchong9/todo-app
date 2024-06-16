822904aa4a0bc070e53c479f8ed42cc1ab550c80
Jeremy Chong <chong.jeremy@hotmail.com>
2024-06-16 15:33:35 -0400

    saved tasks to local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import TaskForm from "./TaskForm";
 import Task from "./Task";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    if (tasks.length === 0)
+      return;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    setTasks(tasks);
+  }, []);
   function addTask(name) {
     setTasks(prev => {
       return [...prev, {name: name, done: false}];
--- src/Task.js
+++ src/Task.js
@@ -3,7 +3,7 @@ import Checkbox from "./Checkbox";
 export default function Task({name, done}) {
     return (
       <div className="task">
-        <Checkbox />
+        <Checkbox defaultChecked={done} />
         {name}
       </div>
     );
