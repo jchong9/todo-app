2710ed2f22f0f358d8642e3025b5a8129bdd6a8c
Jeremy Chong <chong.jeremy@hotmail.com>
2024-06-16 16:40:00 -0400

    added dynamic headings for tasks

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import {useEffect, useState} from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
   useEffect(() => {
     if (tasks.length === 0)
       return;
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }, [tasks]);
+
   useEffect(() => {
     const tasks = JSON.parse(localStorage.getItem('tasks'));
     setTasks(tasks);
   }, []);
+
   function addTask(name) {
     setTasks(prev => {
       return [...prev, {name: name, done: false}];
     });
   }
+
   function updateTaskDone(taskIndex, newDone) {
     setTasks(prev => {
       const newTasks = [...prev];
@@ -26,8 +30,28 @@ function App() {
       return newTasks;
     });
   }
+
+  const numberComplete = tasks.filter(t => t.done).length;
+  const numberTotal = tasks.length;
+
+  function getMessage() {
+    const percentage = numberComplete / numberTotal * 100;
+    switch (percentage) {
+      case 0:
+        return "Let's get it started!";
+      case 50:
+        return "Almost there!";
+      case 100:
+        return "Great job!";
+      default:
+        return "Keep it going!";
+    }
+  }
+
   return (
     <main>
+      <h1>{numberComplete}/{numberTotal} Complete</h1>
+      <h2>{getMessage()}</h2>
       <TaskForm onAdd={addTask} />
       {tasks.map((task, index) => (
         <Task {...task}
